ee8968d9cffd1a1629eec3e5432c0950febdd687
Tim Hårek Andreassen <tim@harek.no>
2026-03-22 19:49:17 +0100

    refactor: Move everything to root to make go install possible

--- cli/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/raskrebs/sonar/cmd"
-
-func main() {
-	cmd.Execute()
-}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/raskrebs/sonar/internal/cmd"
+
+func main() {
+	cmd.Execute()
+}
